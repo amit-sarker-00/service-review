3c9d80c89a9f15e0df9e474b3c79fc9c8874dc49
Amit Sarker <amitsarker255@gmail.com>
2022-11-07 21:09:31 +0600

    env local added for firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwxRgDEl7XQ3-XzxQjOVuwj6x8--fwWNA",
-  authDomain: "wild-photo.firebaseapp.com",
-  projectId: "wild-photo",
-  storageBucket: "wild-photo.appspot.com",
-  messagingSenderId: "382956113983",
-  appId: "1:382956113983:web:69f417ecec16e70fe1a88a",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase

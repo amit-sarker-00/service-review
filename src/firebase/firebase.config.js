7d82a9bcbe8c3031ed04f45ad09caea0067fed5e
Amit Sarker <amitsarker255@gmail.com>
2022-11-07 21:01:50 +0600

    route setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwxRgDEl7XQ3-XzxQjOVuwj6x8--fwWNA",
+  authDomain: "wild-photo.firebaseapp.com",
+  projectId: "wild-photo",
+  storageBucket: "wild-photo.appspot.com",
+  messagingSenderId: "382956113983",
+  appId: "1:382956113983:web:69f417ecec16e70fe1a88a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
